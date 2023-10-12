4f85ab71750c857ebc1c73c53679af17294de686
Cristian Santos <csantosr@unal.edu.co>
2023-10-12 18:33:59 -0500

    Getting along with Integer Partitions

--- 4 kyu/Getting along with Integer Partitions/index.ts
+++ 4 kyu/Getting along with Integer Partitions/index.ts
@@ -1,42 +1,48 @@
-type Partitions = number[][];
-type Memory = Partitions[];
+type Partition = number[];
 
-const recursivePart = (n: number, mem: Memory): Partitions => {
-  if (n === 1) return [[1]];
-  if (mem[n]) {
-    return mem[n];
-  }
-  
-  let partitions: Partitions = [[n]];
-  for(let i = n - 1; i >= 1; i--) {
-    const iParts = recursivePart(n-i, mem)
-      .filter((p) => p[0] <= i)
-      .map((p) => [i, ...p])
-    partitions = partitions.concat(iParts)
-  }
-  mem[n] = partitions;
+function generatePartitions(n: number, max?: number): Partition[] {
+    if (n === 0) return [[]];
+    if (n < 0) return [];
+    if (!max || max > n) max = n;
 
-  return partitions;
+    let partitions: Partition[] = [];
+    
+    for (let i = max; i >= 1; i--) {
+        let subPartitions = generatePartitions(n - i, i);
+        subPartitions.forEach(part => {
+            partitions.push([i, ...part]);
+        });
+    }
+    return partitions;
 }
 
-export const part = (n:number): string => {
+function prod(n: number): number[] {
+    let partitions = generatePartitions(n);
+    let products: { [key: number]: boolean } = {};
 
-  const mem: Memory = Array(n).fill(null);
-  const partitions = recursivePart(n, mem)
-  const products = partitions
-    .map((p) =>
-      p.reduce((acc, curr) => acc*curr, 1)
-    )
-    .filter((prod, index, arr) => arr.indexOf(prod) === index)
-    .sort((a,b) => a-b);
+    partitions.forEach(partition => {
+        let product = partition.reduce((a, b) => a * b, 1);
+        products[product] = true;
+    });
 
-  const range = products[products.length - 1] - products[0];
-  const avg = products.reduce((acc, curr) => acc+curr, 0) / products.length;
-  const median = products.length % 2 === 0
-    ? (products[(products.length/2)-1] + products[((products.length/2) + 1) -1]) / 2
-    : products[((products.length + 1)/2)-1];
-
-  return `Range: ${range} Average: ${avg.toFixed(2)} Median: ${median.toFixed(2)}`
+    return Object.keys(products).map(Number).sort((a, b) => a - b);
 }
 
-console.log(part(10))
+function part(n: number): string {
+    let products = prod(n);
+    let range = products[products.length - 1] - products[0];
+    let average = products.reduce((sum, val) => sum + val, 0) / products.length;
+
+    let median: number;
+    if (products.length % 2 === 1) {
+        median = products[Math.floor(products.length / 2)];
+    } else {
+        let mid1 = products[(products.length / 2) - 1];
+        let mid2 = products[products.length / 2];
+        median = (mid1 + mid2) / 2;
+    }
+
+    return `Range: ${range} Average: ${average.toFixed(2)} Median: ${median.toFixed(2)}`;
+}
+
+console.log(part(5));
